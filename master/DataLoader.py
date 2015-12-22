37b88b629dd0bfdc0de3dd82ee290eebde32b9d3
Michael <glasder.michael@googlemail.com>
2015-12-22 10:51:50 +0100

    Fixed a bug when loading covtype.

--- master/DataLoader.py
+++ master/DataLoader.py
@@ -52,13 +52,13 @@ def load_covtype():
 
     data, target = da.load_svmlight_file("data/covtype/covtype.libsvm.binary", 54)
     target = np.where(target == 1, -1, 1)
-    X, NONE, Y, NONE = cv.train_test_split(data, target, train_size=SAMPLESIZE)
-    x, x_test, y, y_test = cv.train_test_split(X, Y, train_size=1 / 3)
+    X, ups, Y, ups = cv.train_test_split(data, target, train_size=SAMPLESIZE)
+    x, x_test, y, y_test = cv.train_test_split(X, Y, train_size=0.3)
     return x, x_test, y, y_test
 
 
 def load_skin():
-    data, target = da.load_svmlight_file("data/skin-nonskin/skin_nonskin.txt", 54)
+    data, target = da.load_svmlight_file("data/skin-nonskin/skin_nonskin.txt", 3)
     x, x_test, y, y_test = cv.train_test_split(data, target, train_size=1 / 3)
     return x, x_test, y, y_test
 
