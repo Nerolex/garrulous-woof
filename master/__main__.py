f8643efc41bca48cd7d87f5794d43c0f3a0643c0
Michael <glasder.michael@googlemail.com>
2016-01-25 11:09:37 +0100

    l

--- master/__main__.py
+++ master/__main__.py
@@ -250,8 +250,8 @@ def run_batch(data):
 
 # main program
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-run_batch("ijcnn")
-# run_batch("cod-rna")
+# run_batch("ijcnn")
+run_batch("cod-rna")
 # run_batch("skin")
 #run_batch("covtype")
 print("Done!")
