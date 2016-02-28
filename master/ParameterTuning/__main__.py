a7705c572afaafc232fd518263e998ed3bab7765
Michael <glasder.michael@googlemail.com>
2016-02-28 12:31:51 +0100

    Major major restructurations.

--- master/ParameterTuning/__main__.py
+++ master/ParameterTuning/__main__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
+
 import os
 import sys
 import warnings
 
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 import Gridsearcher
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
     data = sys.argv[1]
     # data = "a9a"
     Gridsearcher.gridsearch_and_save(data)
